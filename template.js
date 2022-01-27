dd22a6aeec8514470527c90a07632d640c38c7a5
MyeongHoonNam <codenmh0822@naver.com>
2022-01-27 14:26:05 +0900

    데이터 입력 template 지정

---
+++ template.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const stdin = (
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `입력값 입력`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const solution = () => {};
+
+console.log(solution());
