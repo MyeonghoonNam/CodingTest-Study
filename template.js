9d024fb6df35443dec0d431c8ddc8cc9101d4282
MyeongHoonNam <codenmh0822@naver.com>
2022-01-29 15:23:37 +0900

    template 수정

--- template.js
+++ template.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require("fs");
 const stdin = (
-  process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
-    : `입력값 입력`
-).split('\n');
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `입력값`
+).split("\n");
 
 const input = (() => {
   let line = 0;
