4e2023864aad5f7e235e527b3f514f99ee391f89
MyeongHoonNam <codenmh0822@naver.com>
2022-02-19 01:04:28 +0900

    무한루프문제 해결

--- Sort/quickSort.js
+++ Sort/quickSort.js
@@ -1,5 +1,4 @@
 // 가장 대표적인 호어방식 퀵 정렬 알고리즘 구현
-
 const quickSort = (array, start = 0, end = array.length - 1) => {
   if (start >= end) return; // 원소가 1개인 경우 종료
 
@@ -12,6 +11,7 @@ const quickSort = (array, start = 0, end = array.length - 1) => {
 };
 
 const partition = (array, start, end) => {
+  // console.log("partition start");
   const pivotValue = array[start];
   const pivotIndex = start;
   let left = start + 1;
@@ -19,16 +19,18 @@ const partition = (array, start, end) => {
 
   while (left <= right) {
     // pivot 보다 큰 데이터를 찾기 => 시작 부분 부터
-    while (left <= end && array[left] < pivotValue) start++;
+    while (left <= end && array[left] <= pivotValue) left++;
 
     // pivot 보다 작은 데이터를 찾기 => 끝 부분 부터
-    while (right > start && array[right] > pivotValue) right--;
+    while (right > start && array[right] >= pivotValue) right--;
 
     // index가 엇갈린 경우 작은 데이터(array[right])와 pivot 교체
     if (left > right) {
       swap(array, right, pivotIndex);
     } else {
       swap(array, left, right);
+      left++;
+      right--;
     }
   }
 
