7f802cfe4d7647fd676786709d8bd580868500b4
MyeongHoonNam <codenmh0822@naver.com>
2022-09-16 18:52:03 +0900

    bfs 로직으로 구현

--- Search/음료수얼려먹기.js
+++ Search/음료수얼려먹기.js
@@ -25,7 +25,53 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-const solution = () => {
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = this.tail = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.size === 0) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.size += 1;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return;
+
+    const popNode = this.head;
+    this.head = popNode.next;
+
+    if (this.size === 1) {
+      this.tail = null;
+    }
+
+    this.size -= 1;
+
+    return popNode.value;
+  }
+
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
+}
+
+const solution1 = () => {
   const initGraph = () => {
     const map = new Array(N);
 
@@ -74,4 +120,65 @@ const solution = () => {
   return result;
 };
 
-console.log(solution());
+const solution2 = () => {
+  const initGraph = () => {
+    const graph = new Array(N);
+
+    for (let i = 0; i < N; i++) {
+      const row = input().split("").map(Number);
+      graph[i] = row;
+    }
+
+    return graph;
+  };
+
+  const checkRange = (x, y) => {
+    if (x >= 0 && y >= 0 && x < N && y < M) return true;
+    else return false;
+  };
+
+  const bfs = (x, y) => {
+    const queue = new Queue();
+    queue.enqueue({ x, y });
+    visited[x][y] = true;
+
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+
+    while (!queue.isEmpty()) {
+      const { x, y } = queue.dequeue();
+
+      for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+
+        if (!checkRange(nx, ny)) continue;
+
+        if (graph[nx][ny] === 0 && visited[nx][ny] === false) {
+          queue.enqueue({ x: nx, y: ny });
+          visited[nx][ny] = true;
+        }
+      }
+    }
+  };
+
+  const [N, M] = input().split(" ").map(Number);
+  const graph = initGraph(N, M);
+  const visited = Array.from(new Array(N), () => new Array(M).fill(false));
+
+  let result = 0;
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (graph[i][j] === 0 && visited[i][j] === false) {
+        bfs(i, j);
+        result += 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+// console.log(solution1());
+console.log(solution2());
